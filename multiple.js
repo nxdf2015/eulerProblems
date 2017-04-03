367b6f56aa2b5d9cd2019105db3ed6398bcedbe3
info2016 <niquet_xavier2002@yahoo.fr>
2017-04-03 17:18:16 +0200

    sum multiple.js 1

--- multiple.js
+++ multiple.js
@@ -1,17 +1,14 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 function isMultiple(i,numbers){
-   
-  return   numbers.some(n => i % n == 0);
+     return   numbers.some(n => i % n == 0);
 }
 
 function * multiple(start,end,...numbers){
     let i = start;
-    console.log(numbers);
-    while ( i <= end){
+     while ( i <= end){
         if (isMultiple(i,numbers))
-            {console.log(i);
-            yield i;}
+          yield i; 
             i++;
     }
 }
